78279e02e28377725b76fb76e43c21505b00b1b1
Christian Dechant InTerestedOWL <entwicklung@interestedowl.de>
2024-12-20 10:23:01 +0100

    Reformatted code

--- src/app/components/portfolio.tsx
+++ src/app/components/portfolio.tsx
@@ -12,12 +12,10 @@ export default function Portfolio() {
       ></Title>
 
       <div
-        className="mx-auto grid w-full grid-cols-1 gap-8 pt-12 sm:w-3/4 md:gap-10 lg:w-full lg:grid-cols-2"
-      >
+        className="mx-auto grid w-full grid-cols-1 gap-8 pt-12 sm:w-3/4 md:gap-10 lg:w-full lg:grid-cols-2">
         <Link
           href="/Masterthesis.pdf"
-          className="mx-auto transform transition-all hover:scale-105 md:mx-0"
-        >
+          className="mx-auto transform transition-all hover:scale-105 md:mx-0">
           <img
             src="/LieferanDino.png"
             className="w-full shadow"
@@ -39,38 +37,39 @@ export default function Portfolio() {
             className="w-full shadow"
             alt="portfolio image"
           />
-          <p className="text-grey pt-4 text-sm group-hover:text-white md:text-base">My master&#39;s thesis for the degree
+          <p className="text-grey pt-4 text-sm group-hover:text-white md:text-base">My master&#39;s thesis for the
+            degree
             Master of Applied Computer Science focused on analyzing Kubernetes security architectures and evaluating the
             effectiveness of Zero Trust principles in mitigating vulnerabilities, balancing enhanced protection with
             increased resource consumption and complexity.</p>
         </Link>
-        {/*<Link*/}
-        {/*  href="/"*/}
-        {/*  className="mx-auto transform transition-all hover:scale-105 md:mx-0"*/}
-        {/*>*/}
-        {/*  <img*/}
-        {/*    src="/in-progress.svg"*/}
-        {/*    className="w-96 shadow justify-center content-center"*/}
-        {/*    alt="portfolio image"*/}
-        {/*  />*/}
-        {/*  <p className="text-grey pt-4 text-sm group-hover:text-white md:text-base">*/}
-        {/*    A cool new project with C# .NET and Angular is currently being developed here. It will be a travel planning*/}
-        {/*    software.*/}
-        {/*  </p>*/}
-        {/*</Link>*/}
-        {/*<Link*/}
-        {/*  href="/"*/}
-        {/*  className="mx-auto transform transition-all hover:scale-105 md:mx-0"*/}
-        {/*>*/}
-        {/*  <img*/}
-        {/*    src="/in-progress.svg"*/}
-        {/*    className="w-96 shadow justify-center content-center"*/}
-        {/*    alt="portfolio image"*/}
-        {/*  />*/}
-        {/*  <p className="text-grey pt-4 text-sm group-hover:text-white md:text-base">*/}
-        {/*    A cool new project is currently being developed here.*/}
-        {/*  </p>*/}
-        {/*</Link>*/}
+        {/*<Link*/ }
+        {/*  href="/"*/ }
+        {/*  className="mx-auto transform transition-all hover:scale-105 md:mx-0"*/ }
+        {/*>*/ }
+        {/*  <img*/ }
+        {/*    src="/in-progress.svg"*/ }
+        {/*    className="w-96 shadow justify-center content-center"*/ }
+        {/*    alt="portfolio image"*/ }
+        {/*  />*/ }
+        {/*  <p className="text-grey pt-4 text-sm group-hover:text-white md:text-base">*/ }
+        {/*    A cool new project with C# .NET and Angular is currently being developed here. It will be a travel planning*/ }
+        {/*    software.*/ }
+        {/*  </p>*/ }
+        {/*</Link>*/ }
+        {/*<Link*/ }
+        {/*  href="/"*/ }
+        {/*  className="mx-auto transform transition-all hover:scale-105 md:mx-0"*/ }
+        {/*>*/ }
+        {/*  <img*/ }
+        {/*    src="/in-progress.svg"*/ }
+        {/*    className="w-96 shadow justify-center content-center"*/ }
+        {/*    alt="portfolio image"*/ }
+        {/*  />*/ }
+        {/*  <p className="text-grey pt-4 text-sm group-hover:text-white md:text-base">*/ }
+        {/*    A cool new project is currently being developed here.*/ }
+        {/*  </p>*/ }
+        {/*</Link>*/ }
       </div>
     </div>
   )
